12dc337d0e150b2a996f63d3a2dab789feb713c3
Herman H Hermitage <hermanhermitage@hotmail.com>
2013-07-25 17:37:07 +1000

    Changed frontfacing sample

--- qpu-sniff/fs/frontfacing.fs
+++
@@ -1,9 +0,0 @@
-"uniform vec4 c1;"
-"uniform vec4 c2;"
-"void main(void) {"
-"  if (gl_FrontFacing)"
-"    gl_FragColor = c1;"
-"  else"
-"    gl_FragColor = c2;"
-"}"
-
---
+++ qpu-sniff/fs/gl_FrontFacing.fs
@@ -0,0 +1,6 @@
+"uniform vec4 c1;"
+"uniform vec4 c2;"
+"void main(void) {"
+"  gl_FragColor = vec4(gl_FrontFacing, 1, 1, 1);"
+"}"
+
